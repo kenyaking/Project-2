ace2656db53b6a36e368af7ee6b0d1d527f08c62
elsalanty <elsalanty@gmail.com>
2019-09-07 11:05:45 -0400

    database added

---
+++ config/project-2.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE project2_DB;
+USE project2_DB;
+
+CREATE TABLE players (
+playerid INTEGER NOT NULL AUTO_INCREMENT,
+playername VARCHAR(50) NULL,
+teamid INTEGER NOT NULL,
+points INTEGER NULL,
+weapon1 BOOLEAN NULL,
+PRIMARY KEY (id)
+);
+
+
+
+
+
+
+
+
+
