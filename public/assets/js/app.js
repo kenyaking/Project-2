e2e1a9f904bebabdecf07efea3ffc4ea4aae13f5
Brian Evans <brianevans99@gmail.com>
2019-09-13 23:51:00 -0400

    added javascript to handle player name, avatar; added IDs for game text boxes

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,10 +1,28 @@
 $( document ).ready(function() {
 
+var playerAvatar;
+var playerName;
+
+$("img").on("click", function() {
+    playerAvatar = ($(this).attr("id"));
+  });
 
 
 
+$("button#playGame").on("click", function() {
+    // event.preventDefault();
+    playerName = ($("input").val());
 
+    if (playerName !== "" && playerAvatar !== "") {
+        console.log(playerName + " " + playerAvatar);
+        $("#playerName").text(playerName);
+    } else {
+        event.preventDefault();
+        alert("Please enter your player name and choose an Avatar");
+    }
+    
 
+  });
 
 
 
