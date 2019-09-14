81b87d37f79dd1775ce69bd80f5b575cdd17597a
Brian Evans <brianevans99@gmail.com>
2019-09-14 09:47:56 -0400

    pushing to change computers

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -8,17 +8,26 @@ $("img").on("click", function() {
   });
 
 
+  function getPlayer() {
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM players", function(err, res) {
+      if (err) throw err;
+      
+      connection.end();
+    });
+  }
 
 $("button#playGame").on("click", function() {
     // event.preventDefault();
     playerName = ($("input").val());
 
     if (playerName !== "" && playerAvatar !== "") {
-        console.log(playerName + " " + playerAvatar);
-        $("#playerName").text(playerName);
+        getPlayer();
+        
+
     } else {
         event.preventDefault();
-        alert("Please enter your player name and choose an Avatar");
+        alert("Please enter your player name and choose an avatar");
     }
     
 
